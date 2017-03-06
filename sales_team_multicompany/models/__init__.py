91729dd54649aaa8f4a76051d30dfd3a47eae192
MonsieurB <contact@ebii.fr>
2017-03-06 17:32:47 +0100

    pre init mig V10

--- sales_team_multicompany/models/__init__.py
+++ sales_team_multicompany/models/__init__.py
@@ -2,6 +2,4 @@
 # © 2016 Chafique DELLI @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from.import models
-from .hooks import pre_init_hook
-from .hooks import post_init_hook
+from . import sales_team
